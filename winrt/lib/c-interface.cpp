b8f0e8b3a0c3bb5b7eb73b95b4374665de6b67bd
Cristian Petruta <crispet@microsoft.com>
2019-03-06 23:43:47 -0800

    Add more objects to the C interface to support Canvas2D in HoloJs

--- winrt/lib/c-interface.cpp
+++ winrt/lib/c-interface.cpp
@@ -1,4 +1,5 @@
 #include "pch.h"
+#include <brushes/CanvasLinearGradientBrush.h>
 
 STDAPI getSharedDevice(void** deviceUnknown)
 {
@@ -26,13 +27,43 @@ STDAPI getRenderTarget(void* deviceUnknown,
 {
     auto device = reinterpret_cast<ABI::Microsoft::Graphics::Canvas::ICanvasDevice*>(deviceUnknown);
 
-    auto renderTarget =
-        ABI::Microsoft::Graphics::Canvas::CanvasRenderTarget::CreateNew(device, width, height, dpi, pixelFormat, alphaMode);
+    auto renderTarget = ABI::Microsoft::Graphics::Canvas::CanvasRenderTarget::CreateNew(
+        device, width, height, dpi, pixelFormat, alphaMode);
 
     if (!renderTarget) {
         return E_FAIL;
     }
 
     *renderTargetUnknown = renderTarget.Detach();
+    return S_OK;
+}
+
+STDAPI getCanvasTextFormat(void** canvasTextFormatUnknown)
+{
+    auto canvasTextFormat = Make<ABI::Microsoft::Graphics::Canvas::CanvasTextFormat>();
+    if (!canvasTextFormat) {
+        return E_FAIL;
+    }
+
+    *canvasTextFormatUnknown = canvasTextFormat.Detach();
+    return S_OK;
+}
+
+STDAPI getCanvasLinearGradientBrush(void* rendererUnknown,
+                                    ABI::Microsoft::Graphics::Canvas::Brushes::CanvasGradientStop* stops,
+                                    unsigned int stopCount,
+                                    void** canvasLinearGradientBrushUnknown)
+{
+    auto linearGradientBrushFactory =
+        Make<ABI::Microsoft::Graphics::Canvas::Brushes::CanvasLinearGradientBrushFactory>();
+
+    auto renderer = reinterpret_cast<ABI::Microsoft::Graphics::Canvas::CanvasRenderTarget*>(rendererUnknown);
+
+    ComPtr<ICanvasLinearGradientBrush> linearGradientBrush;
+    if (FAILED(linearGradientBrushFactory->CreateWithStops(renderer, stopCount, stops, &linearGradientBrush))) {
+        return E_FAIL;
+    }
+
+    *canvasLinearGradientBrushUnknown = linearGradientBrush.Detach();
     return S_OK;
 }
